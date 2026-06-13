3b557741becb61d2433a6dda4230f3a0ed91f495
ericfaith005-cyber <ericfaith005@gmail.com>
2026-06-13 10:28:34 +0300

    Imperial Update: Table UI aligned with Executive Grid Standard

--- api/admin.py
+++ api/admin.py
@@ -440,7 +440,7 @@ admin.site.register(SchoolPayLedger, SchoolPayLedgerAdmin)
 
 class FinancialCommandAdmin(admin.ModelAdmin):
     # This path must match your folder structure exactly!
-    change_list_template = "admin/api/financialcommandcenter/change_list.html"
+    change_list_template = "admin/api/financialcommand/change_list.html"
     
     list_display = ('name', 'district', 'current_term_subtotal', 'total_national_revenue')
     
@@ -499,4 +499,4 @@ class FinancialCommandAdmin(admin.ModelAdmin):
         extra_context['pie_data_json'] = json.dumps([mtn, airtel, other])
 
         return super().changelist_view(request, extra_context=extra_context)
-admin.site.register(FinancialCommandCenter, FinancialCommandAdmin)
+admin.site.register(FinancialCommand, FinancialCommandAdmin)
