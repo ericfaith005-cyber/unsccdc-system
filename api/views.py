a1a362fd1fb406eb8f10625bf71d0f63242e7952
ericfaith005-cyber <ericfaith005@gmail.com>
2026-07-06 06:36:46 +0300

    Imperial Alignment: Unified Auth Doors and URL Registry Organization

--- api/views.py
+++ api/views.py
@@ -1,5 +1,5 @@
 import json
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.utils import timezone
