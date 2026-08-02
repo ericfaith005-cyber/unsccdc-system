6adc376bf24a1c7f0486a8270769939d3649dd6d
ericfaith005-cyber <ericfaith005@gmail.com>
2026-08-02 18:44:19 +0300

    Imperial Alignment: Unified Auth Doors and URL Registry Organization

--- UNSCCDC/settings.py
+++ UNSCCDC/settings.py
@@ -147,7 +147,6 @@ JAZZMIN_SETTINGS = {
     "custom_js": None,
     "user_avatar": "photo",
     
-    "custom_css": "css/national_prestige.css", 
     "custom_js": "js/national_flag.js",
     "theme": "darkly",
     "site_brand": "UNSCCDC",
