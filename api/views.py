a2ea7122793980a9e6f381d88e4994d68131748c
ericfaith005-cyber <ericfaith005@gmail.com>
2026-09-18 20:06:55 +0300

    Describe your updates here

--- api/views.py
+++ api/views.py
@@ -8023,7 +8023,9 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 from rest_framework.authtoken.models import Token
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 from .models import Staff, Student, Parent
 from .serializers import UserIdentitySerializer # Import the serializer we just fixed
 
